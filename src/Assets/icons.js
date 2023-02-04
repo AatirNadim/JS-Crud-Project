41220a37ed55939c1fa5590760184255b275bf8f
Aatir Nadim <aatir.nadim@gmail.com>
2023-02-04 13:05:22 +0530

    working on table

---
+++ src/Assets/icons.js
@@ -0,0 +1,19 @@
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+
+export const View = <VisibilityOutlinedIcon
+    sx={{
+        color: '#f33823',
+    }}
+/>
+export const Edit = <BorderColorOutlinedIcon
+    sx={{
+        color: '#f33823',
+    }}
+/>
+export const Delete = <DeleteOutlineOutlinedIcon
+    sx={{
+        color: '#f33823',
+    }}
+/>
--- src/Components/ManageStudent.js
+++ src/Components/ManageStudent.js
@@ -6,49 +6,130 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { backdropClasses, List, ListItem, ListItemButton, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { View, Edit, Delete } from '../Assets/icons';
+
 
 function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+  return { name, calories, fat, carbs };
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Aatir', 159, 6.0, 24, 4.0),
+  createData('Nadim', 237, 9.0, 37, 4.3),
+  createData('Bosdmon', 262, 16.0, 24, 6.0),
+  createData('Cheems', 305, 3.7, 67, 4.3),
+  createData('abdabi', 356, 16.0, 49, 3.9),
 ];
 
+// console.log(rows);
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row",
+
+        }}
+      >
+        <Typography>ADD STUDENT</Typography>
+        <Box>
+          {/* add currernt time in pretty format */}
+          Time
+        </Box>
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead
+              sx={{
+                backgroundColor: '#f33823',
+              }}
             >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              <TableRow
+              >
+                <TableCell align="center"
+                  sx={{
+                    color: '#ffffff',
+                  }}
+                >Name</TableCell>
+                <TableCell align="center"
+                  sx={{
+                    color: '#ffffff',
+                  }}
+                >Class</TableCell>
+                <TableCell align="center"
+                  sx={{
+                    color: '#ffffff',
+                  }}>Roll No.</TableCell>
+                <TableCell align="center"
+                  sx={{
+                    color: '#ffffff',
+                  }}>View/Edit/Delete</TableCell>
+                {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row, idx) => (
+                <TableRow
+                  key={row.name}
+                  sx={{
+                    '&:last-child td, &:last-child th': { border: 0 },
+
+                    backgroundColor: idx % 2 === 0 ? '#fff' : '#fff6f5',
+                    border: 'none',
+                    boxShadow: 'none',
+                  }}
+                >
+                  <TableCell align="center" component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="center">{row.calories}</TableCell>
+                  <TableCell align="center">{row.carbs}</TableCell>
+                  <TableCell align="center">
+                  <List 
+                  sx = {{
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    alignItems  : 'center', 
+                  }}
+                  >
+                    <ListItem disablePadding
+                    sx = {{
+                      // backgroundColor : '#f33823',
+                      width: '100px',
+                    }}
+                    >
+                      {View}
+                    </ListItem>
+                    <ListItem  disablePadding
+                    sx = {{
+                      // backgroundColor : '#f33823',
+                      width: '100px',
+                    }}>
+                      {Edit}
+                    </ListItem>
+                    <ListItem disablePadding
+                    sx = {{
+                      // backgroundColor : '#f33823',
+                      width: '100px',
+                    }}>
+                      {Delete}
+                    </ListItem>
+                  </List>
+
+
+                  </TableCell>
+                  {/* <TableCell align="right">{row.protein}</TableCell> */}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Box>
   );
 }
